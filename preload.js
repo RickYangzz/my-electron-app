8f21f27a16aa10f919b249385353c2820b494084
rick_yang <781195098@qq.com>
2024-04-14 22:31:01 +0800

    feature: communicate between main and renderer processes with Electron's inter-process communication (IPC) modules.

--- preload.js
+++ preload.js
@@ -8,3 +8,13 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
 });
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // we can also expose variables, not just functions
+  ping: () => ipcRenderer.invoke('ping'),
+});
